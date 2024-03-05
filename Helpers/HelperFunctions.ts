0da0e23f0cd2fafc34765e77a11c3d800a50bff7
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2024-03-05 18:34:37 +0300

    home for helper functions

---
+++ Helpers/HelperFunctions.ts
@@ -0,0 +1,24 @@
+// Helper function that converts prices to ugandan currency values
+export function formartPrice(price: number): string {
+  return price.toLocaleString("en-US", {
+    style: "currency",
+    currency: "UGX",
+  });
+}
+
+export function truncateText(text: string, maxLength: number): string {
+  if (text.length > maxLength) {
+    return text.substring(0, maxLength) + "...";
+  }
+  return text;
+}
+
+export function validateEmail(email: string): boolean {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(email.toLowerCase());
+}
+
+export const blurhash =
+  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
