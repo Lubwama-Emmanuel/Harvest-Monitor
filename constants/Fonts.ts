6913610aa58720f260c19e1bf1e6a8d865607ae6
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2024-03-05 18:33:42 +0300

    feat:home for constants

---
+++ constants/Fonts.ts
@@ -0,0 +1,10 @@
+import { moderateScale } from "./Dimension";
+
+const size = {
+  p1: moderateScale(15),
+  p2: moderateScale(13),
+  p3: moderateScale(11),
+  h5: moderateScale(22),
+};
+
+export default size;
---
+++ constants/Measurements.ts
@@ -0,0 +1,6 @@
+import { height, width } from "./Dimension";
+
+export const screenGutter = width * 0.02;
+export const productWidthLarge = width * 0.49;
+export const productWidthSmall = ((width - screenGutter * 2) / 2) * 0.95;
+export const tenth = height * 0.01196;
