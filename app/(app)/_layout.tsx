a76ffb2fce75484ca4b6652183870b3b827f0335
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2024-03-15 06:18:16 +0300

    feat:finished auth

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,15 +1,34 @@
 import { Redirect, Stack } from "expo-router";
 import { Entypo } from "@expo/vector-icons";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/redux/Store";
+import { LogOut } from "@/utils/Authentication";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { clearUser } from "@/redux/slices/AuthSlice";
+import { showNotification } from "@/redux/slices/NotificationSlice";
+import { TouchableOpacity } from "react-native";
 
 export default function AppLayout() {
   const { isLoggedIn } = useSelector((state: RootState) => state.auth);
+
+  const dispatch = useDispatch();
 
   console.log(isLoggedIn);
 
   if (!isLoggedIn) {
     return <Redirect href={"/(auth)/login"} />;
+  }
+
+  async function handleLogOut() {
+    try {
+      await LogOut();
+      await AsyncStorage.removeItem("token");
+      await AsyncStorage.removeItem("userDetails");
+      dispatch(clearUser());
+      dispatch(showNotification("Logged out successfully"));
+    } catch (error) {
+      console.log("Error", error);
+    }
   }
 
   return (
@@ -20,7 +39,11 @@ export default function AppLayout() {
           headerShown: true,
           headerTitle: "Dashboard",
           headerRight: () => {
-            return <Entypo name="log-out" size={24} color="black" />;
+            return (
+              <TouchableOpacity onPress={handleLogOut}>
+                <Entypo name="log-out" size={24} color="black" />
+              </TouchableOpacity>
+            );
           },
         }}
       />
--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -19,6 +19,7 @@ export default function TabOneScreen() {
       },
     });
   }
+
   return (
     <ScrollView
       contentContainerStyle={{
