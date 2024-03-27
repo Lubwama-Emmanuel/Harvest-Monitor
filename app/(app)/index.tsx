3fb7e9497fbcce517322633890982c658cbcab59
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2024-03-27 21:44:18 +0300

    feat:added more feats

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -9,10 +9,148 @@ import { themeColor } from "@/constants/Colors";
 import { useGetDataQuery } from "@/api/firebaseApi";
 import Loader from "@/components/Loader";
 import ValueContainer from "@/components/ValueContainer";
+import { DataType } from "@/types/DataType";
+import moment from "moment";
+
+interface AggregatedData {
+  labels: string[];
+  temperatures: number[];
+  humidities: number[];
+  methanes: number[];
+}
+
+interface AggregatedDataByWeek {
+  labels: string[];
+  temperatures: number[];
+  humidities: number[];
+  methanes: number[];
+}
+
+const processDataForChartByWeek = (
+  data: DataType[]
+): Record<string, AggregatedData> => {
+  const groupedByWeekAndDay: Record<
+    string,
+    Record<
+      string,
+      { temps: number[]; humidities: number[]; methanes: number[] }
+    >
+  > = {};
+
+  data.forEach((item) => {
+    const weekOfYear = moment.unix(item.ts).week(); // Week of the year
+    const dayOfWeek = moment.unix(item.ts).format("dddd"); // 'Monday', 'Tuesday', etc.
+    const key = `${weekOfYear}-${dayOfWeek}`;
+
+    if (!groupedByWeekAndDay[weekOfYear]) {
+      groupedByWeekAndDay[weekOfYear] = {};
+    }
+    if (!groupedByWeekAndDay[weekOfYear][dayOfWeek]) {
+      groupedByWeekAndDay[weekOfYear][dayOfWeek] = {
+        temps: [],
+        humidities: [],
+        methanes: [],
+      };
+    }
+
+    groupedByWeekAndDay[weekOfYear][dayOfWeek].temps.push(item.temp);
+    groupedByWeekAndDay[weekOfYear][dayOfWeek].humidities.push(item.humidity);
+    groupedByWeekAndDay[weekOfYear][dayOfWeek].methanes.push(item.methane);
+  });
+
+  // Calculate averages and prepare data for each week separately
+  const result: Record<string, AggregatedDataByWeek> = {};
+  Object.keys(groupedByWeekAndDay).forEach((week) => {
+    const daysData = groupedByWeekAndDay[week];
+    const labels = Object.keys(daysData);
+    const temperatures = labels.map(
+      (day) =>
+        daysData[day].temps.reduce((acc, curr) => acc + curr, 0) /
+        daysData[day].temps.length
+    );
+    const humidities = labels.map(
+      (day) =>
+        daysData[day].humidities.reduce((acc, curr) => acc + curr, 0) /
+        daysData[day].humidities.length
+    );
+    const methanes = labels.map(
+      (day) =>
+        daysData[day].methanes.reduce((acc, curr) => acc + curr, 0) /
+        daysData[day].methanes.length
+    );
+
+    result[week] = { labels, temperatures, humidities, methanes };
+  });
+
+  return result;
+};
+
+const processDataForChart = (data: DataType[]): AggregatedData => {
+  const groupedByDay: Record<
+    string,
+    { temps: number[]; humidities: number[]; methanes: number[] }
+  > = {};
+
+  // Grouping data
+  data.forEach((item) => {
+    const dayOfWeek = moment.unix(item.ts).format("ddd");
+    if (!groupedByDay[dayOfWeek]) {
+      groupedByDay[dayOfWeek] = { temps: [], humidities: [], methanes: [] };
+    }
+    groupedByDay[dayOfWeek].temps.push(item.temp);
+    groupedByDay[dayOfWeek].humidities.push(item.humidity);
+    groupedByDay[dayOfWeek].methanes.push(item.methane);
+  });
+
+  // Calculating avg temp, hum, meth
+  const labels = Object.keys(groupedByDay);
+  const temparatures = labels.map((day) => {
+    const temps = groupedByDay[day].temps;
+    return temps.reduce((acc, curr) => acc + curr, 0) / temps.length;
+  });
+
+  const humidities = labels.map((day) => {
+    const humidities = groupedByDay[day].humidities;
+    return humidities.reduce((acc, curr) => acc + curr, 0) / humidities.length;
+  });
+
+  const methanes = labels.map((day) => {
+    const methanes = groupedByDay[day].methanes;
+    return methanes.reduce((acc, curr) => acc + curr, 0) / methanes.length;
+  });
+
+  return {
+    labels,
+    temperatures: temparatures.map((t) => parseFloat(t.toFixed(2))),
+    humidities: humidities.map((h) => parseFloat(h.toFixed(2))),
+    methanes: methanes.map((m) => parseFloat(m.toFixed(2))),
+  };
+};
 
 // Main screens
 export default function TabOneScreen() {
-  const { data, isError, isLoading } = useGetDataQuery();
+  const { data, isError, isLoading, refetch } = useGetDataQuery(undefined, {
+    pollingInterval: 10000,
+    skipPollingIfUnfocused: true,
+  });
+
+  let lastedData;
+  let filteredData: DataType[] = [];
+
+  if (data) {
+    const dataArray: DataType[] = Object.values(data);
+    filteredData = dataArray.filter(
+      (item) =>
+        !(item.humidity === 998 && item.methane === 0 && item.temp === 998)
+    );
+    const sortedArray = filteredData.sort((a, b) => a.ts - b.ts);
+    lastedData = sortedArray[filteredData.length - 1];
+  }
+
+  const weekNumber = moment.unix(Date.now() / 1000).week();
+
+  const processedData = processDataForChartByWeek(filteredData);
+  const weekData = processedData[weekNumber];
 
   async function scheduleNotificationHandler() {
     await Notification.scheduleNotificationAsync({
@@ -39,41 +177,34 @@ export default function TabOneScreen() {
       <View style={styles.overAllChart}>
         <ValueContainer
           title="Temparature"
-          value={Number(data?.temp)}
+          value={Number(lastedData?.temp)}
           symbol="°C"
         />
         <ValueContainer
           title="Humidity"
-          value={Number(data?.humidity)}
+          value={Number(lastedData?.humidity)}
           symbol="%"
         />
         <ValueContainer
           title="Methane"
-          value={Number(data?.methane)}
-          symbol=""
+          value={Number(lastedData?.methane)}
+          symbol="Ppm"
         />
       </View>
 
       <Text>Temperature Chart</Text>
       <LineChart
         data={{
-          labels: ["January", "February", "March", "April", "May", "June"],
+          labels: weekData.labels,
           datasets: [
             {
-              data: [
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-              ],
+              data: weekData.temperatures,
             },
           ],
         }}
         width={width * 0.95} // from react-native
         height={height * 0.25}
-        yAxisSuffix="deg"
+        yAxisSuffix="°C"
         yAxisInterval={1} // optional, defaults to 1
         chartConfig={{
           backgroundColor: themeColor,
@@ -101,23 +232,16 @@ export default function TabOneScreen() {
 
       <BarChart
         data={{
-          labels: ["January", "February", "March", "April", "May", "June"],
+          labels: weekData.labels,
           datasets: [
             {
-              data: [
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-              ],
+              data: weekData.humidities,
             },
           ],
         }}
         width={width * 0.95} // from react-native
         height={height * 0.25}
-        yAxisSuffix="deg"
+        yAxisSuffix="%"
         yAxisLabel="" // optional, defaults to 1
         chartConfig={{
           backgroundColor: themeColor,
@@ -139,23 +263,16 @@ export default function TabOneScreen() {
       <Text>Methane Chart</Text>
       <LineChart
         data={{
-          labels: ["January", "February", "March", "April", "May", "June"],
+          labels: weekData.labels,
           datasets: [
             {
-              data: [
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-              ],
+              data: weekData.methanes,
             },
           ],
         }}
         width={width * 0.95} // from react-native
         height={height * 0.25}
-        yAxisSuffix="deg"
+        yAxisSuffix="ppm"
         yAxisInterval={1} // optional, defaults to 1
         chartConfig={{
           backgroundColor: themeColor,
