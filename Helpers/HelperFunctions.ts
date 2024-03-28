6a1bad1bafdcb8739b93c6881f5709792e276cc9
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2024-03-28 21:40:02 +0300

    feat:added more helper functions

--- Helpers/HelperFunctions.ts
+++ Helpers/HelperFunctions.ts
@@ -19,6 +19,3 @@ export function validateEmail(email: string): boolean {
 
   return re.test(email.toLowerCase());
 }
-
-export const blurhash =
-  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
